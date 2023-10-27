7349e9308842ddf3fbe5eae56605f3ed0e5ea661
Alberto Miranda <codealchemist@gmail.com>
2023-10-27 19:26:42 -0300

    Added new exercises and tests!

--- src/exercises/e-0001.js
+++ src/exercises/e-0001.js
@@ -1,7 +1,6 @@
 /**
  * Takes an array of numbers and returns the sum of all numbers that are repeated.
- * 
- * Big O: TODO
+ * Big O: O(n^2).
  * 
  * @param {Array} nums 
  * @returns {int} sum
@@ -14,7 +13,54 @@ export const sumRepeated = (nums) => {
   return sum
 }
 
-// Enhanced version.
-export const sumRepeatedOptimized = (nums) => {
-  // TODO.
+/**
+ * Takes an array of numbers and returns the sum of all numbers that are repeated.
+ * Big O: O(n).
+ * 
+ * @param {Array} nums 
+ * @returns {int} sum
+ */
+export function sumRepeatedOptimized (nums) {
+  const map = {}
+  let sum = 0
+
+  nums.forEach(num => {
+    if (map[num] === 1) {
+      sum += num
+      map[num] = 1
+    }
+    if (map[num] === 0) {
+      sum += num * 2
+      map[num] = 1
+      return
+    }
+    map[num] = 0
+  })
+
+  return sum
+}
+
+/**
+ * Takes an array of numbers and returns the sum of all numbers that are repeated.
+ * Big O: O(n).
+ * Technically this is O(2n) but we drop the constant.
+ * 
+ * @param {Array} nums 
+ * @returns {int} sum
+ */
+export function sumRepeatedOptimized1 (nums) {
+  const map = {}
+  let sum = 0
+
+  nums.forEach(num => {
+    map[num] ??= 0
+    map[num] += 1
+  })
+  nums.forEach(num => {
+    if (map[num] > 1) {
+      sum += num
+    }
+  })
+
+  return sum
 }
